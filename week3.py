782944aedc549111b3d06e75b09791c938519775
Akira Nishii <62619033+akiranishii@users.noreply.github.com>
2023-11-19 17:23:41 -0500

    change

--- week3.py
+++ week3.py
@@ -65,6 +65,28 @@ class Solution:
         return closest_points
     
 
+
+# from typing import List
+
+# class Solution:
+#     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+#         # calc the squared euclid dist.
+#         def distance(point):
+#             x, y = point
+#             return x**2 + y**2
+#         # sort the points by dist. from origin
+#         points.sort(key=distance)
+#         return points[:k]
+
+# '''
+# use the squared distance to avoid computing the square root (not necessary for this) 
+# sorting in place using distance 
+
+# time comp: O(NlogN) where N is the length of points 
+# space comp: O(1) w/ sort done in place 
+# '''
+    
+
 # Longest Substring Without Repeating Characters
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
